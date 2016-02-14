2b494b37cd8f080e95fbfcb291593a08b3a1e8d7
roveref <francoisrovere@gmail.com>
2016-02-15 02:58:14 +0900

    Seperation of the code into several files to have a better repartition of the roles. A global function checking on the attributes to know where to register in the database and an automatic generation of the questionnaire are still to be done

--- qFullSending.php
+++ qFullSending.php
@@ -1,71 +1,26 @@
 <?php 
-
-    function getIP() {
-    $ip = $_SERVER['SERVER_ADDR'];
-
-    if (PHP_OS == 'WINNT'){
-        $ip = getHostByName(getHostName());
-    }
-
-    if (PHP_OS == 'Linux'){
-        $command="/sbin/ifconfig";
-        exec($command, $output);
-        // var_dump($output);
-        $pattern = '/inet addr:?([^ ]+)/';
-
-        $ip = array();
-        foreach ($output as $key => $subject) {
-            $result = preg_match_all($pattern, $subject, $subpattern);
-            if ($result == 1) {
-                if ($subpattern[1][0] != "127.0.0.1")
-                $ip = $subpattern[1][0];
-            }
-        //var_dump($subpattern);
-        }
-    }
-    return $ip;
-}
+  include 'qFunctions.php';
 
 $ClientiP = getIP();
 
-$server = "localhost";
+$date = getDateYMDHIS();
 
-$date = date('Y-m-d H:i:s');
+//$conn = connectDatabase("localhost", "root", "password29","questionnaire");
 
-$options = array("UID" => "root",  "PWD" => "password29", "DB" => "questionnaire");
-
-echo "<p>Trying to access the database...</p>";
-
-$conn = mysqli_connect($server, "root", "password29","questionnaire");
-
+$conn = mysqli_connect("localhost", "root", "password29","questionnaire");
 if (!$conn) 
     die("<pre>".print_r(mysqli_connect_error(), true));
-
 echo "<p>Connection established.</p>";
 
-$sql = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = 'questionnaire' AND table_name = 'general_information'";
-$query = mysqli_query($conn, $sql);
-$assoc = mysqli_fetch_assoc($query);
-$numberColumnsGI = $assoc['COUNT(*)'];
-echo $numberColumnsGI;
-$sql = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = 'questionnaire' AND table_name = 'mechanicalpen'";
-$query = mysqli_query($conn, $sql);
-$assoc = mysqli_fetch_assoc($query);
-$numberColumnsMP = $assoc['COUNT(*)'];
-echo $numberColumnsMP;
-$sql = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = 'questionnaire' AND table_name = 'jewel'";
-$query = mysqli_query($conn, $sql);
-$assoc = mysqli_fetch_assoc($query);
-$numberColumnsJW = $assoc['COUNT(*)'];
-echo $numberColumnsJW;
 
-$sql = "SELECT * FROM general_information";
-$query = mysqli_query($conn, $sql);
+$numberColumnsGI = getNumberColumn("questionnaire","general_information", $conn);
 
-if (!$query)
-    {
-    exit("<pre>" . print_r(mysqli_error($query) , true));
-    }
+$numberColumnsMP = getNumberColumn("questionnaire","mechanicalpen", $conn);
+
+$numberColumnsJW = getNumberColumn("questionnaire","jewel", $conn);
+
+
+    registerInTable("general_information",$_POST, $date, $ClientiP, $numberColumnsGI, $conn);
 
 array_unshift($_POST, $date);
 array_unshift($_POST, $ClientiP);
@@ -73,13 +28,8 @@ $sqlGI = "INSERT INTO general_information VALUES (";
 $sqlMP = "INSERT INTO mechanicalpen VALUES (";
 $sqlJW = "INSERT INTO jewel VALUES (";
 
-echo $sqlGI;
-echo $sqlMP;
-echo $sqlJW;
 
 $last_key = end(array_keys($_POST));
-
-echo $last_key;
 
 foreach($_POST as $key => $element)
     {
@@ -116,15 +66,12 @@ if($key > $numberColumnsGI-2 AND $key <= $numberColumnsGI+$numberColumnsMP-5)
     if($key > $numberColumnsGI+$numberColumnsMP-5) 
     {
         $var = $numberColumnsGI+$numberColumnsMP-4;
-        echo $key;
-        echo $var;
             if ($key != $last_key AND $key != $numberColumnsGI+$numberColumnsMP-4)
         {
         $sqlJW = $sqlJW . "'" . $element . "',";
         }
             elseif($key == $numberColumnsGI+$numberColumnsMP-4)
         {
-            echo 'yes';
         $sqlJW = $sqlJW . "'" . $_POST['0'] . "'," . $_POST['1'] . "',";
         }
         elseif($key == $last_key)
@@ -134,43 +81,6 @@ if($key > $numberColumnsGI-2 AND $key <= $numberColumnsGI+$numberColumnsMP-5)
     }
     }
 
-
-echo '<p>' . $sqlGI . '</p>';
-echo '<p>' . $sqlMP . '</p>';
-echo '<p>' . $sqlJW . '</p>';
-
-$query = mysqli_query($conn, $sqlGI);
-
-if (!$query){  
-    exit("<pre>".print_r(mysqli_error($query), true));
-}
-else
-{
-        echo "<p>Results of the general part added successfully to the database.</p>";
-}
-
-$query = mysqli_query($conn, $sqlMP);
-
-if (!$query){  
-    exit("<pre>".print_r(mysqli_error($query), true));
-}
-else
-{
-        echo "<p>Results of the mechical pen part added successfully to the database.</p>";
-}
-
-$query = mysqli_query($conn, $sqlJW);
-
-if (!$query){  
-    exit("<pre>".print_r(mysqli_error($query), true));
-}
-else
-{
-        echo "<p>Results of the intermediate jewel part added successfully to the database.</p>";
-}
-
-mysqli_close($conn);
-
-echo "<p>Connection closed.</p>";
+disconnectDatabase($conn);
 
 ?>
---
+++ qFunctions.php
@@ -0,0 +1,97 @@
+<?php  
+   function getIP() {
+    $ip = $_SERVER['SERVER_ADDR'];
+
+    if (PHP_OS == 'WINNT'){
+        $ip = getHostByName(getHostName());
+    }
+
+    if (PHP_OS == 'Linux'){
+        $command="/sbin/ifconfig";
+        exec($command, $output);
+        // var_dump($output);
+        $pattern = '/inet addr:?([^ ]+)/';
+
+        $ip = array();
+        foreach ($output as $key => $subject) {
+            $result = preg_match_all($pattern, $subject, $subpattern);
+            if ($result == 1) {
+                if ($subpattern[1][0] != "127.0.0.1")
+                $ip = $subpattern[1][0];
+            }
+        //var_dump($subpattern);
+        }
+    }
+    return $ip;
+}
+    function getDateYMDHIS() {
+        $date = date('Y-m-d H:i:s');
+    return $date;
+}
+
+function getNumberColumn($database,$table, $connection) {
+$sql = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = '$database' AND table_name = '$table'" ;
+//$sql = "SELECT * FROM $table";
+
+echo  "<p>" . $sql . "</p>";
+$query = mysqli_query($connection, $sql);
+$assoc = mysqli_fetch_assoc($query);
+$numberColumns = $assoc['COUNT(*)'];
+return $numberColumns;
+}
+
+function connectDatabase($host,$user,$password,$database)
+{
+echo "<p>Trying to access the database...</p>";
+
+$connectDatabase = mysqli_connect($host, $user, $password,$database);
+if (!$conn) 
+    die("<pre>".print_r(mysqli_connect_error(), true));
+echo "<p>Connection established.</p>";
+
+return $connectDatabase;
+}
+
+function disconnectDatabase($connection)
+{
+mysqli_close($connection);
+
+echo "<p>Connection closed.</p>";
+}
+
+function registerInTable($table, $data, $date, $clientIP, $numberColumnsGI, $connection)
+    {
+        array_unshift($data, $date);
+        array_unshift($data, $clientIP);
+        $sql = "INSERT INTO $table VALUES (";
+        $last_key = end(array_keys($data));
+
+        foreach($data as $key => $element)
+    {
+        if($key <= $numberColumnsGI-2) 
+        {
+    if ($key != $last_key AND $key != $numberColumnsGI-2)
+        {
+        $sql = $sql . "'" . $element . "',";
+        }
+    else
+        {
+        $sql = $sql . "'" . $element . "',NULL)";
+        }
+    }
+
+    }
+    echo '<p>' . $sql . '</p>';
+
+    $query = mysqli_query($connection, $sql);
+
+if (!$query){  
+    exit("<pre>".print_r(mysqli_error($query), true));
+}
+else
+{
+        echo "<p>Results of the general part added successfully to the database.</p>";
+}
+
+    }
+?>
